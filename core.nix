7b88c909993f42408f4daaeb40a6140827263096
Joris <joris@jorisvandijk.com>
2026-07-26 22:27:26 +0200

    Adding scaffolding

---
+++ core.nix
@@ -0,0 +1,6 @@
+{ self, ... }:
+{
+  nixpkgs.hostPlatform = "aarch64-darwin";
+  system.stateVersion = 6;
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+}
--- flake.nix
+++ flake.nix
@@ -8,39 +8,17 @@
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-
-      # Touch ID for sudo.
-      security.pam.services.sudo_local.touchIdAuth = true;
-    };
-  in
   {
     # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
+    # $ darwin-rebuild build --flake .#macbook
     darwinConfigurations."macbook" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      specialArgs = { inherit inputs self; };
+      modules = [
+        ./core.nix
+        ./nix.nix
+        ./packages.nix
+        ./security.nix
+      ];
     };
   };
 }
